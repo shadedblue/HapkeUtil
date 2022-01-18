6cc2e3235dd7b9736440d4dc2afcf37fc5b97081
Nate Hapke <nathan@hapke.ca>
2022-01-17 21:18:19 -0800

    #126: User : Birthday missing
    Task-Url: http://github.com/shadedblue/campingbot/issues/126

--- ca.hapke.util/src/main/java/ca/hapke/util/CollectionUtil.java
+++ ca.hapke.util/src/main/java/ca/hapke/util/CollectionUtil.java
@@ -1,7 +1,10 @@
 package ca.hapke.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Nathan Hapke
@@ -41,5 +44,45 @@ public abstract class CollectionUtil {
 		if (length == 0)
 			return -1;
 		return (int) (Math.random() * length);
+	}
+
+	private static final String PERSON_REGEX = "\\([A-Za-z]+\\)";
+	private static final Pattern PERSON = Pattern.compile(PERSON_REGEX);
+
+	public static String search(String searchTerm, List<String> hypes) {
+		searchTerm = searchTerm.toLowerCase();
+		List<String> possible = new ArrayList<>();
+		List<String> possiblePunchlines = null;
+		boolean punchline = searchTerm.startsWith("(") && searchTerm.endsWith(")");
+
+		if (punchline) {
+			possiblePunchlines = new ArrayList<>();
+		}
+
+		for (String s : hypes) {
+			String x = s.toLowerCase();
+			if (x.contains(searchTerm)) {
+				possible.add(s);
+
+				if (punchline) {
+					try {
+						Matcher m = PERSON.matcher(x);
+						String last = null;
+						while (m.find()) {
+							int groupCount = m.groupCount();
+							last = m.group(groupCount);
+						}
+						if (last != null && last.equals(searchTerm))
+							possiblePunchlines.add(s);
+					} catch (IllegalStateException e) {
+					}
+				}
+			}
+		}
+
+		if (punchline && possiblePunchlines.size() > 0) {
+			return getRandom(possiblePunchlines);
+		}
+		return getRandom(possible);
 	}
 }
